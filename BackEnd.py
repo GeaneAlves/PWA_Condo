1264f37fb7919c0b102f5963183215ff4a827897
Geane Alves <geanedarc2@hotmail.com>
2026-08-16 03:44:26 -0300

    salvamento

--- BackEnd.py
+++ BackEnd.py
@@ -1,11 +1,45 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import flask_cors
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name_)
 CORS(app)      # link de acesso do Front End JavaScrip à API
 
-@app.route('/')
-def home():
+
+# Substituir com a URL do  banco PostgreSQL real (local ou nuvem como Supabase/Neon/Render)
+# Formato: postgresql://usuario:senha@host:porta/nome_do_banco
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:suasenha@localhost:5432/condoconnect_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(APP)
+
+# Exemplo de como será a Tabela de Jardinagem
+class Jardinagem(db.Model):
+    __tablename__ = 'jardinagem'
+
+    id = db.Column(db.Integer, primary_key=True)
+    atividade = db.Column(db.String(150), nullable=False)
+    data_agendada = db.Column(db.Date, nullable=False)
+    status = db.Column(db.String(50), default='Agendado')
+    observacao = db.Column(db.Text, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "atividade": self.atividade,
+            "data": self.data_agendada,strftime('%y-%m-%d'),
+            "status": self.status,
+            "observacao": self.observacao
+        }
+
+# 
+@app.route('/api/jardinagem', methods=['GET'])
+def get_jardinagem():
+    eventos = Jardinagem.query.order_by(Jardinagem.data_agendada.asc()).all()
+    return jsonify
+
     return jsonify({"status": "API do COndomínio rodando com sucesso!"})
 
 # Rota inicial do calendário de jardinagem
