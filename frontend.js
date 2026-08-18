ea16060c4404fe0d45e48913f65137793c8dee6b
Geane Alves <geanedarc2@hotmail.com>
2026-08-18 00:25:12 -0300

    salvamento front

--- frontend.js
+++ frontend.js
@@ -1,27 +1,19 @@
 const CACHE_NAME = 'condo-pwa-v1';
 const urlsToCache = [
-    '/',
-    '/frontend/index.html',
-    '/frontend/assets/css/style.css',
-    '/frontend/assets/js/app.js'
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/app.js'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waiUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-    );
-});
-
-self.addEventListener('install', (event) => {
-    event.waiUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-    );
-
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
-
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
